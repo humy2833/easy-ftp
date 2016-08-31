f651ef76ffd1c796e5fb5ad174a79ad175ddb243
unknown <humy2833@joycity.com>
2016-08-31 18:04:46 +0900

    sftp 다중 접속시 한 서버로만 접속되는 버그 수정

--- test/easy-ftp-test.js
+++ test/easy-ftp-test.js
@@ -15,27 +15,47 @@ var config2 = {
 
 var EasyFTP = require('../lib/easy-ftp.js');
 
-var ftp = new EasyFTP();
-ftp.on("open", function(client){
-	console.log("connect", ftp.isFTP);
+var ftp1 = new EasyFTP();
+var ftp2 = new EasyFTP();
+ftp1.on("open", function(client){
+	console.log("connect1", ftp1.isFTP);
 });
-ftp.on("error", function(err){
-	console.log("error", err);
+ftp1.on("error", function(err){
+	console.log("error1", err);
+    end();
 });
-ftp.on("close", function(){
-	console.log("close");
+ftp1.on("close", function(){
+	console.log("close1");
 });
-ftp.on("upload", function(path){
-	console.log("upload", path);
-});
-ftp.on("download", function(path){
-	console.log("download", path);
+ftp1.connect(config2);
+ftp1.ls("/storage", function(err, path){
+	console.log(err, "ftp1", path.length);
+	end();
 });
 
-ftp.connect(config2);
-
-
-ftp.pwd(function(err, path){
-	console.log(err, path);
-	ftp.close();
+ftp2.on("open", function(client){
+	console.log("connect2", ftp2.isFTP);
 });
+ftp2.on("error", function(err){
+	console.log("error2", err);
+    end();
+});
+ftp2.on("close", function(){
+	console.log("close2");
+});
+ftp2.connect(config1);
+ftp2.ls("/storage", function(err, path){
+	console.log(err, "ftp2", path.length);
+	end();
+});
+
+var count = 0;
+function end(){
+    count++;
+    if(count == 2)
+    {
+        console.log(ftp1 == ftp2);
+        try{ftp1.close();}catch(e){}
+        try{ftp2.close();}catch(e){}
+    }
+}
