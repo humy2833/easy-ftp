1fcff0a37d85472f45770acee97732df816a4001
YoungJae Lee <humy2833@naver.com>
2016-09-08 17:30:42 +0900

    ftp 모듈 변경 및 sftp 버그 수정

--- test/easy-ftp-test.js
+++ test/easy-ftp-test.js
@@ -7,26 +7,31 @@ var config1 = {
     path: "/"
 };
 
+var loop = require('easy-loop');
 var EasyFTP = require('../lib/easy-ftp.js');
 
 var ftp1 = new EasyFTP();
 //var ftp2 = new EasyFTP();
 ftp1.on("open", function(client){
-	console.log("connect1", ftp1.isFTP);
+	console.log("event open");
+    end();
+});
+ftp1.on("upload", function(){
+	console.log("event upload", arguments);
+});
+ftp1.on("download", function(){
+	console.log("event download", arguments);
 });
 ftp1.on("error", function(err){
-	console.log("error1", err);
+	console.log("event error1", err);
     end();
 });
 ftp1.on("close", function(){
-	console.log("close1");
+	console.log("event close1");
 });
 ftp1.connect(config1);
-ftp1.ls(config1.path, function(err, path){
-	console.log(err, path ? path.length : "");
-	end();
-});
+
 
 function end(){
-    try{ftp1.close();console.log("close");}catch(e){}
+    try{ftp1.close();}catch(e){}
 }
